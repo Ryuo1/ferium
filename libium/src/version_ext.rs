33b8f5a22d4de4fdb051cdff56bab73e29ab7f11
Ryuo1 <77712873+Ryuo1@users.noreply.github.com>
2026-05-12 02:58:24 +0530

    Update version_ext.rs

--- libium/src/version_ext.rs
+++ libium/src/version_ext.rs
@@ -3,20 +3,34 @@ use ferinth::structures::version::{Version, VersionFile};
 pub trait VersionExt {
     /// Gets the primary (or first) version file of a version
     fn get_version_file(&self) -> &VersionFile;
+
     /// Consumes and returns the primary (or first) version file of a version
     fn into_version_file(self) -> VersionFile;
 }
 
 impl VersionExt for Version {
     fn get_version_file(&self) -> &VersionFile {
+        if self.files.is_empty() {
+            println!("================ EMPTY FILES DETECTED ================");
+            println!("{:#?}", self);
+            panic!("Version has no files");
+        }
+
         self.files
             .iter()
             .find(|f| f.primary)
-            .unwrap_or(&self.files[0])
+            .unwrap_or_else(|| &self.files[0])
     }
 
     fn into_version_file(mut self) -> VersionFile {
+        if self.files.is_empty() {
+            println!("================ EMPTY FILES DETECTED ================");
+            println!("{:#?}", self);
+            panic!("Version has no files");
+        }
+
         let fallback = self.files.swap_remove(0);
+
         self.files
             .into_iter()
             .find(|f| f.primary)
